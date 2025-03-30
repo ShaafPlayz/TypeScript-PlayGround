d625c3278fc51d7b13839051217bfcc44ab1e12c
Shaaf Yousaf <muhdshaaf@gmail.com>
2025-03-30 02:22:39 -0400

    Rename

--- Proj/renderer.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-window.addEventListener('DOMContentLoaded', function () {
-    var output = document.getElementById('count');
-    setInterval(function () {
-        var date = new Date().toLocaleTimeString();
-        output.textContent = date;
-    }, 1000);
-});
--- Proj/renderer.ts
+++
@@ -1,9 +0,0 @@
-
-window.addEventListener('DOMContentLoaded', () =>  {
-    const output = document.getElementById('count') as HTMLHeadingElement;
-
-    setInterval(() => {
-        const date = new Date().toLocaleDateString();   
-        output.textContent = date;
-    }, 1000);
-});
